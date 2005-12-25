a375e2869f3ab99b22c9ee3cb46150f86f73cd54
danielreinert <danielreinert>
2005-12-25 08:27:26 +0000

    Modifications for embedding a java-window in the native plugin-window. Daniel Reinert

--- src/jvst/wrapper/VSTPluginGUIAdapter.java
+++ src/jvst/wrapper/VSTPluginGUIAdapter.java
@@ -9,6 +9,9 @@ import jvst.wrapper.gui.*;
 
 public abstract class VSTPluginGUIAdapter extends JFrame implements VSTPluginGUI {
 
+  static public boolean libraryOk=false;
+  public boolean WindowAttached;
+     
   public VSTPluginGUIAdapter() {
     //center window
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -29,8 +32,39 @@ public abstract class VSTPluginGUIAdapter extends JFrame implements VSTPluginGUI
 
     //IMPORTANT: hide window
     this.hide();
+        
+    //Prepare attaching window
+    WindowAttached=false;
+    if(!libraryOk) {
+      //Load library for native-awt methods 
+      try{
+        System.loadLibrary("jawt");
+        libraryOk=true;          
+      } catch(Exception e){libraryOk=false;}
+      catch(Error e){
+          //Maybe library is already loaded
+          if(e.getMessage().indexOf("already loaded")!=-1) libraryOk=true;
+          else libraryOk=false;
+      }
+    }
 
     log("\nJAVA GUI Plugin intitialised properly!");
+  }
+  
+  public void undecorate()
+  {               
+        if((this.WindowAttached)&&(!this.isUndecorated())) {
+            javax.swing.JFrame f = new javax.swing.JFrame();
+            f.pack();            
+            this.dispose();
+            this.setUndecorated(true);
+            this.pack();
+            f.dispose();
+            java.awt.Rectangle bounds=this.getBounds();
+            bounds.x=0;
+            bounds.y=0;
+            this.setBounds(bounds);            
+        } 
   }
 
   //*********************************************************************
