caff92ed03f1ebf62ac321cac2d9457130462f1b
daniel309 <daniel309>
2008-09-28 12:16:19 +0000

    separate .sh files for each platform now

---
+++ dist/release_win.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+mvn clean
+
+rm -f fb.jpg*
+rm -f style.css*
+rm -f ChangeLog.txt*
+
+export CVSROOT=:pserver:anonymous@jvstwrapper.cvs.sourceforge.net:/cvsroot/jvstwrapper
+mkdir target
+cd target
+
+cvs login
+cvs co jvst_wrapper
+cvs co jvst_system
+cvs co jvst_examples
+cvs co jvst_native
+
+cp ../cvs2cl.pl .
+./cvs2cl.pl -f../ChangeLog.txt
+
+cd ..
+
+wget -O READ_ME.html http://jvstwrapper.sourceforge.net/index.html
+wget http://jvstwrapper.sourceforge.net/style.css
+wget http://jvstwrapper.sourceforge.net/fb.jpg
+
+mvn install javadoc:jar package assembly:assembly
+
+#mvn deploy
+
+echo -n "Dont forget to update the mvn repo at http://jvstwrapper.sf.net/maven2"
+
+#mvn release:prepare
+#mvn release:perform
