d2938b8dc707fdd632b501cb0720241fb7277844
daniel309 <daniel309>
2009-04-12 13:29:20 +0000

    refactored: generic jvst stuff (logging, initialization) is now in VSTPluginAdapter instead of VSTV10ToHost, makes much more sense now...

--- src/jvst/wrapper/VSTPluginAdapter.java
+++ src/jvst/wrapper/VSTPluginAdapter.java
@@ -27,6 +27,10 @@
 package jvst.wrapper;
 
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.text.DecimalFormat;
 
 import jvst.wrapper.communication.VSTV24ToHost;
@@ -50,13 +54,21 @@ import jvst.wrapper.valueobjects.VSTVariableIO;
  */
 public abstract class VSTPluginAdapter extends VSTV24ToHost implements VSTV24ToPlug {
 
-  //Constants
+
+  //C++ pointer to the plugin instance
+  protected long WrapperInstance = 0;
+  
+  private static String logBasePath = "c:/";
+  private static String logFileName = "JAVA_STD_OUT.txt";
+  private static boolean logEnabled = false;
+  
+  
+  
+  //VST Constants
   //************************************
   public static final int PAN_LAW_LINEAR = 0;	  // L = pan * M; R = (1 - pan) * M;
   public static final int PAN_LAW_EQUAL_POWER = 1;// L = pow (pan, 0.5) * M; R = pow ((1 - pan), 0.5) * M;
 
-  //C++ pointer to the plugin instance
-  protected long WrapperInstance = 0;
   
   public VSTPluginAdapter(long Wrapper) {
   	this.WrapperInstance=Wrapper;
@@ -240,5 +252,68 @@ public abstract class VSTPluginAdapter extends VSTV24ToHost implements VSTV24ToP
     DecimalFormat form = new DecimalFormat("#0.0#");
     return form.format(value);
   }  
+  
+  
+  
+  //jVSTwRapper java-side init and log stuff
+  //**************
+
+  /**
+   *  is called by the plugin wrapper lib BEFORE plugins construktor!
+   */
+  protected static void _initPlugFromNative(String dllLocation, boolean log) {
+    File nativeLib = new File(dllLocation);
+  	if (nativeLib.isDirectory() || !nativeLib.exists()) 
+  		throw new IllegalArgumentException("Illegal native lib location: '" + dllLocation + "'");
+    
+  	logBasePath = nativeLib.getParent();
+    logEnabled = log;
+
+    String dllFileName = nativeLib.getName();
+    int point = dllFileName.lastIndexOf(".");
+    if (point == -1) {
+      throw new IllegalStateException("Could not find '.' in dllFileName " + dllFileName);
+    }
+    String LibName = dllFileName.substring(0, point);
+    logFileName = LibName + "_java_stdout.txt";
+
+    try {
+      File logf = new File(new File(logBasePath), logFileName);
+      PrintStream str = new PrintStream(new BufferedOutputStream(new FileOutputStream(logf, true)));
+      System.setOut(str);
+      System.setErr(str);
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      throw new IllegalStateException(e.toString());
+    }
+
+    log("registering native Library '" + dllLocation + "'");
+    System.load(dllLocation); //this load() gets delegated to the VSTiClassLoader, 
+    						  //so that native calls get dispatched correctly!
+  }
+  
+
+  /**
+   * logging
+   */
+  public static void log(String s) {
+    if (isLogEnabled()) {
+      System.out.println(s);
+      System.out.flush();
+    }
+  }
+
+  protected static boolean isLogEnabled() {
+    return logEnabled;
+  }
+
+  protected static String getLogBasePath() {
+    return logBasePath;
+  }
+
+  protected static String getLogFileName() {
+    return logFileName;
+  }
   
 }
--- src/jvst/wrapper/communication/VSTV10ToHost.java
+++ src/jvst/wrapper/communication/VSTV10ToHost.java
@@ -37,10 +37,6 @@ import java.io.*;
  */
 public abstract class VSTV10ToHost  {
 
-  private static String logBasePath = "c:/";
-  private static String logFileName = "JAVA_STD_OUT.txt";
-  private static boolean logEnabled = false;
-
 
   //vst 1.0 REQUIRED
   //****************
@@ -92,67 +88,5 @@ public abstract class VSTV10ToHost  {
   public native void masterIdle();
   public native boolean isInputConnected(int input);
   public native boolean isOutputConnected(int output);
-
-
-  //dm custom
-  //**************
-
-  /**
-   *  is called by the plugin wrapper lib BEFORE plugins construktor!
-   */
-  protected static void _initPlugFromNative(String dllLocation, boolean log) {
-    File nativeLib = new File(dllLocation);
-  	if (nativeLib.isDirectory() || !nativeLib.exists()) 
-  		throw new IllegalArgumentException("Illegal native lib location: '" + dllLocation + "'");
-    
-  	logBasePath = nativeLib.getParent();
-    logEnabled = log;
-
-    String dllFileName = nativeLib.getName();
-    int point = dllFileName.lastIndexOf(".");
-    if (point == -1) {
-      throw new IllegalStateException("Could not find '.' in dllFileName " + dllFileName);
-    }
-    String LibName = dllFileName.substring(0, point);
-    logFileName = LibName + "_java_stdout.txt";
-
-    try {
-      File logf = new File(new File(logBasePath), logFileName);
-      PrintStream str = new PrintStream(new BufferedOutputStream(new FileOutputStream(logf, true)));
-      System.setOut(str);
-      System.setErr(str);
-    }
-    catch (Exception e) {
-      e.printStackTrace();
-      throw new IllegalStateException(e.toString());
-    }
-
-    log("registering native Library '" + dllLocation + "'");
-    System.load(dllLocation); //this load() gets delegated to the VSTiClassLoader, 
-    						  //so that native calls get dispatched correctly!
-  }
-  
-
-  /**
-   * logging
-   */
-  public static void log(String s) {
-    if (isLogEnabled()) {
-      System.out.println(s);
-      System.out.flush();
-    }
-  }
-
-  protected static boolean isLogEnabled() {
-    return logEnabled;
-  }
-
-  protected static String getLogBasePath() {
-    return logBasePath;
-  }
-
-  protected static String getLogFileName() {
-    return logFileName;
-  }
 
 }
